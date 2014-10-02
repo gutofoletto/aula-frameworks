a3b1e3422f06995dcb86c7ce491983ac1beafd1b
Antonio Augusto Foletto <gutofoletto@webfield.com.br>
2014-10-02 13:10:42 -0300

    adicionando a estrutura de base

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Configuração do Projeto
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    compass: {
+        css: {
+            options:{
+                config: 'config.rb'
+            }
+        }
+    },
+    cssmin:{
+        minificar: {
+            files:{
+                'assets/css/framework.min.css':'assets/css/framework.css'
+            }
+        }
+    },
+    watch: {
+        dev: {
+            files: ['src/sass/**/*'],
+            tasks: ['compass', 'cssmin']
+        }
+    }
+
+  });
+
+  // Carrega as tarefas
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Define comandos para tarefas.
+  grunt.registerTask('default', ['']);
+  grunt.registerTask('dev', ['watch']);
+
+};
